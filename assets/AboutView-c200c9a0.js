1445669235fb4b9b651687938696cfe6a8501653
Shadow Reaper <313828474@qq.com>
2023-04-22 16:10:15 +0800

    1

---
+++ assets/AboutView-c200c9a0.js
@@ -0,0 +1,1 @@
+import{r as e,g as t,h as s,i as o}from"./index-2de9d684.js";const a={class:"about"},_=o("h1",null,"About Us",-1),c=[_],l={__name:"AboutView",setup(r){return e("/"),e(""),(n,i)=>(t(),s("div",a,c))}};export{l as default};
--- assets/index-219d6607.js
+++
@@ -1,1 +0,0 @@
-import{S as e,V as r}from"./index-9120e9aa.js";const a=e(r);export{a as r};
